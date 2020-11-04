377c4b28dee5b784016c494c0ffeb2560715348d
tusihao@hzsuidifu.com <tusihao@hzsuidifu.com>
2020-11-04 19:21:43 +0800

    update

--- utils/timeUtil.go
+++ utils/timeUtil.go
@@ -1,5 +1,7 @@
 package utils
 
+import "time"
+
 /*
 @Author : VictorTu
 @Software: GoLand
@@ -10,3 +12,9 @@ type timeUtil struct {
 
 var TimeUtil timeUtil
 
+func (t *timeUtil) FormatTime(layout string, d time.Time) (dString string) {
+	if !d.IsZero() {
+		dString = d.Format(layout)
+	}
+	return
+}
