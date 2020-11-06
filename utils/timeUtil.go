aa662af33fdf218b53b9018f672ae3098a2e8f6a
tusihao@hzsuidifu.com <tusihao@hzsuidifu.com>
2020-11-06 13:19:18 +0800

    update

--- utils/timeUtil.go
+++ utils/timeUtil.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 /*
 @Author : VictorTu
@@ -12,9 +15,23 @@ type timeUtil struct {
 
 var TimeUtil timeUtil
 
+const (
+	Layout_Date_Time9      = "2006-01-02 15:04:05.999999999"
+	Layout_Date_Time6      = "2006-01-02 15:04:05.999999"
+	Layout_Date_Time       = "2006-01-02 15:04:05"
+	Layout_Date            = "2006-01-02"
+	Layout_Date0           = "20060102"
+	Time_Layout_Date_Time2 = "20060102150405"
+)
+
 func (t *timeUtil) FormatTime(layout string, d time.Time) (dString string) {
 	if !d.IsZero() {
 		dString = d.Format(layout)
 	}
+	return
+}
+
+func (t *timeUtil) BeginTimeOfDay(d time.Time) (nd time.Time) {
+	nd, _ = time.ParseInLocation(Layout_Date_Time, fmt.Sprintf("%s 00:00:00", d.Format(Layout_Date)), time.Local)
 	return
 }
